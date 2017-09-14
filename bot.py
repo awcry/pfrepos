881de5b9da799e8e3afc0e3f089a512dc305a233
awcry <fleeex.g@gmail.com>
2017-09-14 14:13:57 +0300

    '134'

--- bot.py
+++ bot.py
@@ -14,9 +14,9 @@ from PIL import Image
 
 bot = telebot.TeleBot(config.token)
 
-WEBHOOK_HOST = 'www.pfpgupsbot.herokuapp.com'
+WEBHOOK_HOST = 'pfpgupsbot.herokuapp.com'
 WEBHOOK_PORT = 80  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = 'www.pfpgupsbot.herokuapp.com'  # На некоторых серверах придется указывать такой же IP, что и выше
+WEBHOOK_LISTEN = 'pfpgupsbot.herokuapp.com'  # На некоторых серверах придется указывать такой же IP, что и выше
 
 WEBHOOK_SSL_CERT = 'webhook_cert.pem'  # Путь к сертификату
 WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'  # Путь к приватному ключу
